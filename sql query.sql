682398cb7ec66346edcc8cbe2e932999a3321bf4
jasonnnnn128 <jasonoueng96@gmail.com>
2026-03-24 10:35:11 +0700

    Add files via upload
    This program is a CLI-based (Command Line Interface) bank transaction management application developed using Python and MySQL as a relational database. The system is designed to simulate basic banking operations, including customer registration, cash deposits, cash withdrawals, transfers, and balance inquiries, in a secure and consistent manner.

---
+++ sql query.sql
@@ -0,0 +1,15 @@
+create database yourdatabasename
+use yourdatabasename
+
+CREATE TABLE yourtablename (
+    kode_antrian VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    pin VARCHAR(10) NOT NULL,
+    balance INT NOT NULL
+);
+
+select * from yourtablename
+
+
+
+
